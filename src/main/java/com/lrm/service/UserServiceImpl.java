854a55366469743beaf5cdf7aa73c3535bc0476b
hanxiaochuang666 <623233529@qq.com>
2019-07-04 17:38:34 +0800

    我的个人博客项目

---
+++ src/main/java/com/lrm/service/UserServiceImpl.java
@@ -0,0 +1,22 @@
+package com.lrm.service;
+
+import com.lrm.dao.UserRepository;
+import com.lrm.po.User;
+import com.lrm.util.MD5Utils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by hc on 2017/10/15.
+ */
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public User checkUser(String username, String password) {
+        return userRepository.findByUsernameAndPassword(username, MD5Utils.code(password));
+    }
+}
